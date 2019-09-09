90b878257f2c086571e5921c528af0a77500cf11
邹振 <zhen_zou@sui.com>
2019-09-09 18:54:09 +0800

    feat:nil interface

--- struct/nil.go
+++ struct/nil.go
@@ -12,8 +12,40 @@ func (n *Nil) Panic() {
 	println("Panic:", n.Name)
 }
 
+type People interface {
+	Show()
+}
+
+type Student struct{}
+
+func (stu *Student) Show() {
+
+}
+
+func live1() People {
+	var stu *Student
+	return stu
+}
+
+func live2() *Student {
+	var stu *Student
+	return stu
+}
+
 func main() {
 	var n *Nil
+
+	if live1() == nil {
+		println("AAAAAAA")
+	} else {
+		println("BBBBBBB")
+	}
+
+	if live2() == nil {
+		println("AAAAAAA")
+	} else {
+		println("BBBBBBB")
+	}
 
 	n.NotPanic()
 	n.Panic()
