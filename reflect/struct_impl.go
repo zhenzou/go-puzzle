2c98502db02e157f33bd0576c946297a04a89490
september <zzzhen1994@gmail.com>
2020-04-06 14:14:37 +0800

    feat: impl puzzled

---
+++ reflect/struct_impl.go
@@ -0,0 +1,30 @@
+package main
+
+import "reflect"
+
+type Wrapper interface {
+	NativeType() reflect.Type
+}
+
+type Person struct {
+	Name string
+}
+
+func (person *Person) Fuck() string {
+	return "fuck"
+}
+
+func (person *Person) NativeType() reflect.Type {
+	return reflect.TypeOf("fuck")
+}
+
+var wrapperType = reflect.TypeOf((*Wrapper)(nil)).Elem()
+
+func main() {
+	// 指针实现Wrapper
+	p := Person{}
+
+	println(reflect.TypeOf(p).Implements(wrapperType))
+
+	println(reflect.PtrTo(reflect.TypeOf(p)).Implements(wrapperType))
+}
